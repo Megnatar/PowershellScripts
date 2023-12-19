2fd64f43207c9bd493370c8ca0ea23b47aca6d78
Megnatar <Megnatar@users.noreply.github.com>
2023-12-19 18:00:28 +0100

    Update Add New AD User.ps1

--- Add New AD User.ps1
+++ Add New AD User.ps1
@@ -19,7 +19,7 @@ $Domain = If ($TestOmgeving) {"@sometst.nl"} Else {"@somedomain.nl"}            
 $NewUser = $ExampleUser = $ChangeNumber = $ExpirationDate = $NewAccount = ''            # maakt al deze variabele weer leeg, zodra je het script opnieuw start. Voorkomt problemen bij een restart en is nodig voor while loops.
 $ExpirationTime = '00:00:00'                                                            # 20:00:00 8 uur in de avond. Tijd is opioneel en kan gebruikt worden.
 $ProfilePath = '\\Some.Domain\SOMECOMPANYNAME\users\'                                   # Path naar alle gebruikers profielen (RUPs), en is zonder de UPN naam.
-$HomeDrive = 'Z'                                                                        # De drive waar alle profielen op staan.
+$HomeDrive = 'X'                                                                        # De drive waar alle profielen op staan.
 $Today = get-Date -Format 'yy-MM-dd'                                                    # De datum van vandaag. Wordt gebruikt als voorbeeld voor het invullen van de datum.
 
 # Array met ongeldige groepen. Ongeldige groepen zijn groepen die appart moeten worden aangevraagd of niet meer van toepassing zijn.
