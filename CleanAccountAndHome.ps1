69b610df02bb724fd89be7065ec4c7683d6f4c91
Megnatar <Megnatar@users.noreply.github.com>
2024-02-10 15:05:29 +0000

    Update CleanAccountAndHome.ps1

--- CleanAccountAndHome.ps1
+++ CleanAccountAndHome.ps1
@@ -1,35 +1,38 @@
 <#
-    Accounts Opruimen V1.13
+    Accounts Opruimen V1.2
+
 
     Dit script ruimt de accounts en home folders op van medewerkers die een maand of langer uit dienst zijn.
-    Het doet dit door de datum uitdienst van een account op te halen uit het omschijving veld in het account.
-    Vul het daarom altijd goed in!
+    Het Script wordt automatisch 1 keer per week uitgevoerd up de SomeServer via een schedule task.
 
-    Het maakt de 'Domain admins' groep eigenaar van de home folder en gooit hem daarna weg.
-    Alle groepslidmaatschappen van de gebruiker worden verwijderd.
-    Bij het verwijderen wordt ook de Office365 licentie verwijderd, daarmee wordt ook de mailbox van de bebruiker weggegooid.
-    Vervolgens wordt het account uit de tussenfase OU gehaald en in de _Disabled Accounts OU geplaatst van het huidige jaar.
+    Het maakt de 'Domain admins' groep eigenaar van de home folder en gooit hem daarna weg, als deze bestaat.
+    Alle groepslidmaatschappen van de gebruiker worden verwijderd door het script, behalve de Domain users groep.
+    Bij het verwijderen wordt ook de Office365 licentie verwijderd, daarmee wordt ook de mailbox van de gebruiker weggegooid.
+    Vervolgens wordt het account uit de tussenfase OU gehaald en in de _Disabled Accounts OU geplaatst, van het huidige jaar.
     
-    Er wordt een e-mail verzonden naar ictinfo@stadgenoot.nl met alle aanpassingen.
+    Er wordt als laatst een e-mail verzonden naar SomeEmail@SomeDomain.org met alle aanpassingen.
+    Als er geen aanpassingen zijn, dan wordt er geen mail verzonden.
 
     Geschreven door: Jos Severijnse™.
 #>
 
 # Globale Variabelen.
-$AdminGroup = "DOMAIN\'Domain admins"
-$OuWaitOneMonth = "OU=OneMothPause, OU=Disabled Accounts,OU=Business,DC=Domain,DC=SubDomain"
-$OuThisYear = "OU=Disabled Accounts,OU=Disabled Accounts,OU=Business,DC=Domain,DC=SubDomain"
-$MessageBody = @("Disabeled accounts and deleted home folders`n___________________________________________________`n`n")
+$AdminGroup = "CONNECT\'Domain admins"
+$OuTussenfase = "OU=OneMothPause, OU=Disabled Accounts,OU=Business,DC=Domain,DC=local"
+$Year = 2024
+$OuThisYear = "OU=_Disabled Accounts $Year,OU=Disabled Accounts,OU=Business,DC=Domain,DC=local"
 $OneMonthAgo = Get-Date (([datetime]::ParseExact((Get-Date -Format "yy-MM-dd"), 'yy-MM-dd', $null)).AddDays(-30)) -format "yy-MM-dd"
+$MessageBodySend = $MessageBody = @("De volgende accounts en home folders zijn opgeruimt:`n___________________________________________________`n`n")
 
-# Gebruikers ophalen uit de OU en loop door alle gevonden accounts.
-Get-ADUser -SearchBase $OuWaitOneMonth  -filter * -Properties HomeDirectory, mailNickName, Description | % {
+# Gebruikers ophalen uit de OU, en 'loop' door alle gevonden accounts. $mailNickName kan er uitgehaald worden.
+# Maar omdat een bepaald sleutelwoord meerdere properties kan ophalen, moet dat nog even getest worden.
+Get-ADUser -SearchBase $OuTussenfase  -filter * -Properties HomeDirectory, mailNickName, Description | % {
 
-    # Variabelen met betrekking tot het huidige account in de loop.
+    # Variabelen met betrekking tot het huidige account in de 'loop'.
     $Homedir = $_.HomeDirectory
     $SamAccountname = $_.SamAccountname
-    $Mailnickname = $_.MailNickName
     $DateEndOfContract = $_.Description.Substring(0, 8)
+    # $Mailnickname = $_.MailNickName
 
     # Is de gebruiker al een maand uit dienst?
     if ($DateEndOfContract -lt $OneMonthAgo) {
@@ -45,10 +48,10 @@ Get-ADUser -SearchBase $OuWaitOneMonth  -filter * -Properties HomeDirectory, mai
             # Vraag alle mappen op in de home folder van de gebruiker.
             $Folders = Get-ChildItem $Homedir -Directory -Recurse
 
-            # loop door alle mappen en maak de 'Domain admins' groep eigenaar.
+            # 'loop' door alle mappen en maak de 'Domain admins' groep eigenaar.
             Foreach($Folder in $Folders) {
 
-                # Maakt de 'Domain admins' groep eigenaar van alle folders in de home folder.
+                # Maakt de 'Domain admins' groep eigenaar van de huidige map in de 'loop'.
                 $Acl = Get-Acl $Folder.FullName
                 $Acl.SetOwner([System.Security.Principal.NTAccount]"$AdminGroup")
                 Set-Acl $Folder.FullName $Acl -Verbose
@@ -57,16 +60,15 @@ Get-ADUser -SearchBase $OuWaitOneMonth  -filter * -Properties HomeDirectory, mai
            # Vraag alle bestanden op in de home folder van de gebruiker.
             $Files = Get-ChildItem $Homedir -File -Recurse
 
-            # loop door alle bestanden en maak de 'Domain admins' groep eigenaar.
+            # 'loop' door alle bestanden en maak de 'Domain admins' groep eigenaar.
             Foreach($File in $Files) {
 
-                # Maakt de 'Domain admins' groep eigenaar van alle files in de home folder.
+                # Maakt de 'Domain admins' groep eigenaar van het huidige bestand in de 'loop'.
                 $Acl = Get-Acl $File.FullName
                 $Acl.SetOwner([System.Security.Principal.NTAccount]"$AdminGroup")
                 Set-Acl $File.FullName $Acl -Verbose
             }
-
-            # Verwijder de home folder van de user.
+            # Nu is de domein admins groep eigenaar en kunnen wij de home folder Verwijder.
             Remove-Item $Homedir -Recurse -Force
 
         } else {
@@ -74,11 +76,10 @@ Get-ADUser -SearchBase $OuWaitOneMonth  -filter * -Properties HomeDirectory, mai
             # Als de home folder al verwijderd was, laat dit dan zien in de log.
             $Homedir = "De home folder van deze gebruiker is niet gevonden!"
         }
-
         # Haal alle namen van de groepslidmaatschappen op.
         $GroupMembership = (Get-ADPrincipalGroupMembership $SamAccountname | select name).name
 
-        # Loop door alle gevonden groepen in het account.
+        # 'loop' door alle gevonden groepen in het account.
         Foreach ($group in $GroupMembership) {
             
             # Verwijder alle groepen van het account, maar laat de 'Domain users' groep staan.
@@ -89,27 +90,35 @@ Get-ADUser -SearchBase $OuWaitOneMonth  -filter * -Properties HomeDirectory, mai
                 Remove-ADGroupMember -Identity $group -Members $SamAccountname -Confirm:$false
             }
         }
-
         # Haal de user uit tussenfase en verplaats hem naar de OU voor accounts van users uit dienst.
         get-aduser -identity $SamAccountname | Move-ADObject -TargetPath $OuThisYear 
 
-        # Hou bij welke accounts en home folders er zijn opgeschoond.
-        $MessageBody += "$SamAccountname`n$Homedir`n`n"
+        # Hou bij welke accounts en home folders er worden opgeschoond.
+        $MessageBodySend += "`n___________________________________________________`n`nAccount $SamAccountname is verplaatst naar:`n$OuThisYear`n`nDe volgende Home folder is verwijderd:`n$Homedir`n"
     }
 }
 
-# e-mail opmaak naar Support@SomeDomain.org met een lijst van de wijzigingen.
-$Email = @{
-    From = "SomeServer@SomeDomain.org"
-    To = "Support@SomeDomain.org"
-    SmtpServer = "smtp.SomeDomain.org"
-    Subject = "Disabled Accounts and deleted home folders."
-    Body = [string]$MessageBody
+# Stuur de e-mail alleen als er wijzigen zijn!
+If ([string]$MessageBody -ne [string]$MessageBodySend) {
+
+    # e-mail opmaak naar SomeEmail@SomeDomain.org met een lijst van de wijzigingen.
+    $Email = @{
+        From = "SomeServer@SomeDomain.org"
+        To = "SomeEmail@SomeDomain.org"
+        SmtpServer = "smtp.someDomain.org"
+        Subject = "Accounts die opgeruimd zijn"
+        Body = [string]$MessageBodySend
+    }
+    # Stuur de e-mail!
+    Send-MailMessage @Email
+
+    # Leeg het scherm en laat alle aanpassingen zien.
+    cls
+    $MessageBody
+
+} else {
+
+    # Leeg het scherm en laat bericht zien.
+    cls
+    "Er zijn geen wijzigen gedaan!"
 }
-
-# Stuur de e-mail!
-Send-MailMessage @Email
-
-# Leeg het scherm en laat alle aanpassingen zien.
-cls
-$MessageBody
